25e70535a8f759af5020753da2fc34382ec7d18b
student <student@Achievers-iMac.local>
2017-09-14 11:18:30 +0530

    Initial Commit

---
+++ AutolayOut/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutolayOut
+//
+//  Created by student on 9/14/17.
+//  Copyright © 2017 ABC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
